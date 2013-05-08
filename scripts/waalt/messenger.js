660b4b2abc0b38a202bfa8f94daa5e6ba0bc8102
Adán Sánchez de Pedro Crespo <adansdpc@waalt.com>
2013-05-08 22:23:27 +0200

    Allow succesful send when starting offline

--- scripts/waalt/messenger.js
+++ scripts/waalt/messenger.js
@@ -203,7 +203,7 @@ function Messenger(){
 		var stamp = date.getUTCFullYear()+"-"+("0"+(date.getUTCMonth()+1)).slice(-2)+"-"+("0"+(date.getUTCDate())).slice(-2)+"T"+("0"+(date.getUTCHours())).slice(-2)+":"+("0"+(date.getUTCMinutes())).slice(-2)+":"+("0"+(date.getUTCSeconds())).slice(-2)+"Z";
 		var localstamp = date.getFullYear()+"-"+("0"+(date.getMonth()+1)).slice(-2)+"-"+("0"+(date.getDate())).slice(-2)+"T"+("0"+(date.getHours())).slice(-2)+":"+("0"+(date.getMinutes())).slice(-2)+":"+("0"+(date.getSeconds())).slice(-2)+"Z";
 		if(text.length){
-			var newMessage = new this.Message(Strophe.getBareJidFromJid(app.xmpp.connection.jid), jid, text, html, localstamp);
+			var newMessage = new this.Message(Strophe.getBareJidFromJid(app.xmpp.me.jid), jid, text, html, localstamp);
 			this.list[this.list.length-1].messages.push(newMessage);
 			$("section#chat>article#one div#text").empty();
 			this.state = "active";
