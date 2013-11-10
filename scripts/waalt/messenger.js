loqui/im

commit d814ee64df8a877cbda7a6945a9af9a10b8a919d

Partial fix for #85

scripts/waalt/messenger.js
@@ -32,11 +32,11 @@ var Messenger = {
         stamp: localstamp
       });
       msg.send();
-      $$("section#chat div#text").empty();
+      $$("section#chat div#text").empty().focus();
       App.audio('sent');
     }
     $('section#chat article#main button#plus').show();
-    $('section#chat article#main button#say').hide();
+    $('section#chat article#main button#say').hide().focus();
   },
   
   csn: function (state) {

scripts/waalt/account.js
@@ -191,6 +191,9 @@ var Account = function (core) {
   }
   
   // Bring account to foreground
+  this.focus = function () {
+    $("section#chat div#text").focus();
+  }
   this.show = function () {
     $('section#main').data('user', this.core.user);
     $('section#main').data('provider', this.core.provider);

commit e257bc4de2a0a63e95cf262d6941b190f8fc9aa1

Messages were not being sent properly because of JS bug introduced by d814ee6 (related to #85)

scripts/waalt/messenger.js
@@ -19,7 +19,7 @@ var Messenger = {
         stamp: Tools.localize(Tools.stamp())
       });
       msg.send();
-      $$("section#chat div#text").empty().focus();
+      $("section#chat div#text").empty();
       App.audio('sent');
     }
     $('section#chat article#main button#plus').show();