8934d3482ad899d0c3d2bc3b0e0c5fc0df980d0e
Giovanny Gongora <gioyik@gmail.com>
2013-11-30 15:34:25 -0500

    A little fixes for #111 - CC'ing #95

--- scripts/waalt/account.js
+++ scripts/waalt/account.js
@@ -191,7 +191,7 @@ var Account = function (core) {
   // List all contacts for this account
   this.contactsRender = function () {
     if (document.getElementById('searchForm') == null) {
-        var searchForm = $('<fieldset id=\'searchForm\'><input type="search" placeholder="search"></fieldset>');
+        var searchForm = $('<fieldset id=\'searchForm\'><input type=\'search\' data-l10n-placeholder=\'Searchbar\'></fieldset>');
         var searchFormOnKeyUp = function (event) {
             var target = $(event.target);
             var key = target.val().toUpperCase();
