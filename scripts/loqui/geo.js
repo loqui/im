e0165721ab02a477ae5169b7dfd4efeef6f6840d
Adán Sánchez de Pedro Crespo <adansdpc@waalt.com>
2014-01-24 03:23:43 +0100

    Add Geo class

---
+++ scripts/loqui/geo.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var Geo = {
+
+  curPos: null,
+  cb: null,
+
+  Pos: function (lat, long, accuracy) {
+    this.lat = lat;
+    this.long = long;
+    this.accuracy = accuracy;
+  },
+  
+  posGet: function (cb) {
+    if (navigator && navigator.geolocation) {
+      Lungo.Notification.show('map-marker', _('Geolocating'));
+      navigator.geolocation.getCurrentPosition(Geo.success, Geo.error, {
+        enableHighAccuracy : true,
+        timeout : 30000
+      });
+    } else {
+      Tools.log('Geolocation is not supported.');
+    }
+    Geo.cb = cb;
+  },
+  
+  success: function (pos) {
+    Geo.curPos = new Geo.Pos(pos.coords.latitude, pos.coords.longitude, pos.coords.accuracy);
+    Lungo.Notification.hide();
+    Tools.log("You are in " + Geo.curPos.lat + "N " + Geo.curPos.long + "E.\n Accuracy is " + Geo.curPos.accuracy + "m");
+    Geo.cb(Geo.curPos);
+  },
+  
+  error: function (err) {
+    if (err.code == 1) {
+      Tools.log('The user denied the request for location information.')
+    } else if (err.code == 2) {
+      Tools.log('Your location information is unavailable.')
+    } else if (err.code == 3) {
+      Tools.log('The request to get your location timed out.')
+    } else {
+      Tools.log('An unknown error occurred while requesting your location.')
+    }
+    Lungo.Notification.hide();
+  },
+  
+  distance: function (a, b) {
+    var R = 6371; // km
+    var dLat = (b[0]-a[0]).toRad();
+    var dLon = (b[1]-a[1]).toRad();
+    var lat1 = a[0].toRad();
+    var lat2 = b[0].toRad();
+    var ta = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var tc = 2 * Math.atan2(Math.sqrt(ta), Math.sqrt(1-ta)); 
+    var d = R * tc;
+    return d;
+  }
+  
+}
+
+/** Converts numeric degrees to radians */
+if (typeof(Number.prototype.toRad) === "undefined") {
+  Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+  }
+}
