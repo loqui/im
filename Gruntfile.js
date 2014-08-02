bc7811719e0e66f7d23e46ddd4f405c8adc34956
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2014-08-02 13:53:46 -0500

    a new workspace

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,94 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     meta: {
-      build: "dist"
+      build: "dist",
+      compress: "package"
+    },
+    copy: {
+      srcToFirefoxos: {
+        expand: true,
+        cwd: 'src/',
+        src: '**',
+        dest: '<%= meta.build %>/firefoxos/',
+        flatten: true,
+        filter: 'isFile'
+      },
+      firefoxosToDist: {
+        expand: true,
+        cwd: 'platform/firefoxos/',
+        src: '**',
+        dest: '<%= meta.build %>/firefoxos/',
+        flatten: true,
+        filter: 'isFile'
+      },
+      srcToUbuntuTouch: {
+        expand: true,
+        cwd: 'src/',
+        src: '**',
+        dest: '<%= meta.build %>/ubuntu-touch/www',
+        flatten: true,
+        filter: 'isFile'
+      },
+      ubuntutouchToDist: {
+        expand: true,
+        cwd: 'platform/ubuntu-touch/',
+        src: '**',
+        dest: '<%= meta.build %>/ubuntu-touch/',
+        flatten: true,
+        filter: 'isFile'
+      },
+      srcToDesktop: {
+        expand: true,
+        cwd: 'src/',
+        src: '**',
+        dest: '<%= meta.build %>/desktop/',
+        flatten: true,
+        filter: 'isFile'
+      },
+      desktopToDist: {
+        expand: true,
+        cwd: 'platform/desktop/',
+        src: '**',
+        dest: '<%= meta.build %>/desktop/',
+        flatten: true,
+        filter: 'isFile'
+      }
+    },
+    clean: {
+      build: {
+        src: ['<%= meta.build %>/*', '<%= meta.compress %>/*', '<%= meta.build %>/**/*']
+      }
+    },
+    compress: {
+      firefoxos: {
+        options: {
+          archive: '<%= meta.package %>/LoquiIM-<%= pkg.version %>-firefoxos.zip'
+        },
+        files: [{
+          expand: true, 
+          cwd: '<%= meta.build %>/firefoxos/', 
+          src: ['**'], 
+          dest: ''
+        }]
+      },
+      ubuntutouch: {
+        options: {
+          archive: '<%= meta.package %>/LoquiIM-<%= pkg.version %>-ubuntutouch.zip'
+        },
+        files: [{
+          expand: true, 
+          cwd: '<%= meta.build %>/ubuntu-touch/', 
+          src: ['**'], 
+          dest: ''
+        }]
+      }
+    },
+    nodewebkit: {
+      options: {
+        platforms: ['win', 'osx', 'linux'],
+        buildDir: './package/desktop',
+      },
+      src: ['./platform/desktop'] 
     },
     connect: {
       server: {
@@ -10,7 +97,7 @@ module.exports = function(grunt) {
           port: 3000,
           protocol: 'http',
           hostname: 'localhost',
-          base: '.',
+          base: '/src',
           debug: false,
           livereload: true
         }
@@ -21,7 +108,7 @@ module.exports = function(grunt) {
         livereload: true
       },
       html: {
-        files: ['*'],
+        files: ['src/*', 'src/**/*'],
         options: {
           spawn: false
         }
@@ -30,5 +117,10 @@ module.exports = function(grunt) {
   });
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.registerTask('default', ['clean', 'copy', 'compress', 'nodewebkit']);
+  grunt.registerTask('devel', ['connect', 'watch']);
 };
