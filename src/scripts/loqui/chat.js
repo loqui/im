loqui/im

commit 2857d13a856cd13e28cd40c86bf6f819b9c8f574

fix for #532

src/scripts/loqui/chat.js
@@ -115,7 +115,7 @@ var Chat = function (core, account) {
     var chunkListSize = this.core.chunks.length;
     var blockIndex = this.core.chunks[chunkListSize - 1];
     var storageIndex;
-    chat.core.last = msg;
+
     if (chunkListSize > 0) {
       Store.recover(blockIndex, function (chunk) {
         if (!chunk || chunk.length >= App.defaults.Chat.chunkSize) {
@@ -126,17 +126,22 @@ var Chat = function (core, account) {
           chat.core.chunks.push(blockIndex);
           storageIndex = [blockIndex, 0];
           callback(blockIndex);
+
         } else {
           Tools.log('FITS');
           chunk.push(msg);
           chunk.sort(function (a, b) {
             return Tools.unstamp(a.stamp) > Tools.unstamp(b.stamp);
           });
+
           blockIndex = chat.core.chunks[chunkListSize - 1];
           Tools.log('PUSHING', blockIndex, chunk);
           Store.update(blockIndex, chunk, callback);
           storageIndex = [blockIndex, chunk.length-1];
         }
+
+        chat.core.last = chunk[chunk.length-1];
+
         if (delay) {
           chat.account.toSendQ(storageIndex);
         }

commit c6864728119a509a7d6759f15d5825653454ecec

initial message from a new contact doesn't trigger a notification and isn't shown in the preview, breakage from commit 2857d13a856cd13e28cd40c86bf6f819b9c8f574 for issue #532

src/scripts/loqui/chat.js
@@ -175,6 +175,7 @@ var Chat = function (core, account) {
       Tools.log('PUSHING', blockIndex, chunk);
       chat.core.chunks.push(blockIndex);
       storageIndex = [blockIndex, 0];
+      chat.core.last = msg;
       if (delay) {
         chat.account.toSendQ(storageIndex);
       }