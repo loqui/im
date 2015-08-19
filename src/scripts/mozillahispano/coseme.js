4f2b98790f71aa646d92c95be3b35188aa51e5d9
Christof Meerwald <cmeerw@cmeerw.org>
2015-08-19 06:42:29 +0100

    updated WhatsApp token data

--- src/scripts/mozillahispano/coseme.js
+++ src/scripts/mozillahispano/coseme.js
@@ -3364,10 +3364,10 @@ CoSeMe.namespace('config', (function(){
     groupDomain: 'g.us',
 
     tokenData: {
-      "v": "2.12.88",
+      "v": "2.12.96",
       // XXX: it is tokenData[d] + - + tokenData[v] + - + port
-      "r": "S40-2.12.88-443",
-      "u": "WhatsApp/2.12.88 S40Version/14.26 Device/Nokia302",
+      "r": "S40-2.12.96-443",
+      "u": "WhatsApp/2.12.96 S40Version/14.26 Device/Nokia302",
       "d": "S40"
     },
 
@@ -5784,7 +5784,7 @@ CoSeMe.namespace('registration', (function(){
   'use strict';
 
   function getToken(phone) {
-    var plain = 'PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1435097139726' + phone;
+    var plain = 'PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1439921524310' + phone;
     var data = CryptoJS.enc.Latin1.parse(plain);
     var output = CryptoJS.MD5(data);
     return output.toString();
