05477b3b8ee4fcf98f622ccab54ee72c54a5f216
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2015-09-17 21:57:43 +0200

    fix chats order #818

--- src/scripts/loqui/account.js
+++ src/scripts/loqui/account.js
@@ -728,8 +728,8 @@ var Account = function (core) {
   this.save = function () {
     var index = Accounts.find(this.core.fullJid || this.core.user);
       this.core.chats.sort(function(a, b){
-        a = new Date(a.last.stamp);
-        b = new Date(b.last.stamp);
+        a = new Date(a.last ? a.last.stamp : 0);
+        b = new Date(b.last ? b.last.stamp : 0);
         return a.getTime() > b.getTime();
       });
 
