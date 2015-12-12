ce8d2038e9dd2eb2ea2ba1a38f5ed95e4d65e76d
Sascha Werth <shaigan@web.de>
2015-12-12 20:57:13 +0100

    fix breaking of media rendering

--- src/scripts/loqui/message.js
+++ src/scripts/loqui/message.js
@@ -376,7 +376,7 @@ var Message = {
     var html= null;
     var onDivClick= null;
     if (this.core.text) {
-      html = App.emoji[Providers.data[this.account.core.provider].emoji].fy(Tools.urlHL(Tools.HTMLescape(this.core.text)));
+      html = App.emoji[Providers.data[this.account.core.provider].emoji].fy(Tools.urlHL(Tools.HTMLescape(this.core.text))).replace(/<br>/gi,"");
     } else if (this.core.media) {
       html = $('<img/>').attr('src', this.core.media.thumb);
       html[0].dataset.url = this.core.media.url;
@@ -502,7 +502,7 @@ var Message = {
     }
 
     if (html) {
-      var textSpan = $('<span/>').addClass('text').html(html.replace(/<br>/gi,""));
+      var textSpan = $('<span/>').addClass('text').html(html);
       div.append(textSpan);
     }
 
