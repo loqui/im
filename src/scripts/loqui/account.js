0ae9e3c88e6823d47d25c952d42644df5b48f9d7
Christof Meerwald <cmeerw@cmeerw.org>
2016-03-13 21:12:04 +0000

    fix sorting when there are empty chats (with no last message) in the account

--- src/scripts/loqui/account.js
+++ src/scripts/loqui/account.js
@@ -927,8 +927,8 @@ var Account = {
   save : function () {
     var index = Accounts.find(this.core.fullJid || this.core.user);
       this.core.chats.sort(function(a, b){
-        a = new Date(a.last ? a.last.stamp : 0);
-        b = new Date(b.last ? b.last.stamp : 0);
+        a = new Date((a.last && a.last.stamp) ? a.last.stamp : 0);
+        b = new Date((b.last && b.last.stamp) ? b.last.stamp : 0);
         return a.getTime() > b.getTime();
       });
 
