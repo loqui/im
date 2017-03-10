8aea1403d0bd07dca0c305c8ac7260885b5c2900
Bjarne Roß <nfsprodriver@users.noreply.github.com>
2017-03-10 03:36:55 +0100

    Fix UT UA

--- src/scripts/tapquo/quo.js
+++ src/scripts/tapquo/quo.js
@@ -526,7 +526,7 @@
       Blackberry: /(BlackBerry|BB10|Playbook).*Version\/([\d.]+)/,
       FirefoxOS: /(Mozilla).*(\(Mobile)[^\/]*\/([\d\.]*)/,
       webOS: /(webOS|hpwOS)[\s\/]([\d.]+)/,
-      UbuntuTouch: /(Mozilla).*(Ubuntu; Mobile)[^\/]*\/([\d\.]*)/
+      UbuntuTouch: /(WhatsApp).*Version\/([\d.]+)/
     };
     $$.isMobile = function() {
       _current = _current || _detectEnvironment();
