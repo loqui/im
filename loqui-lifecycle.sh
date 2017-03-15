30daef18852006bc6ec78eff371a197ab70611d4
Bjarne Roß <nfsprodriver@users.noreply.github.com>
2017-03-16 00:48:36 +0100

    Added UT lifecycle script

---
+++ loqui-lifecycle.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+
+SCHEMA="com.canonical.qtmir"
+KEY="lifecycle-exempt-appids"
+
+STATUS=$(gsettings get ${SCHEMA} ${KEY})
+
+if [[ $STATUS == *"'loquiim.nfsprodriver'"* ]]; then
+	echo "LoquiIM is already in the list of apps being able to run in background!"
+  else
+    gsettings set ${SCHEMA} ${KEY} "${STATUS%]*}, 'loquiim.nfsprodriver']"
+	echo "Added LoquiIM to the list of apps being able to run in background."
+fi
